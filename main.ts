e649ee2805cfb64b1b4e13cddd48a5bb376c7cfa
Surya <123630128+SiriGPT@users.noreply.github.com>
2024-07-25 11:58:46 +1200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.forever(function () {
+	
+})
